5f6a7015eb40da179283c7888bbbe6c28707e465
devlynhaytema1 <devlynhaytema@yahoo.ca>
2026-05-14 01:07:56 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ level=logging.INFO,
 format="%(asctime)s [%(levelname)s] %(message)s",
 datefmt="%Y-%m-%d %H:%M:%S",
 )
-log = logging.getLogger__name__)
+log = logging.getLogger__name__
 
 client = ClobClient(
 host=CLOB_HOST,
